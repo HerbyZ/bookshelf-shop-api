9d0c3f5e2286f01373b79bb5beb4ce6d6263a32e
HerbyZ <andrey.tsuranoff@yandex.ru>
2022-01-17 20:54:03 +0300

    Custom User model

--- bookshop/accounts/models.py
+++ bookshop/accounts/models.py
@@ -1,3 +1,60 @@
+from email.policy import default
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+NEW_USER_BONUS_BALANCE = 10
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, bonus_balance=NEW_USER_BONUS_BALANCE):
+        user = self.model(
+            email=self.normalize_email(email),
+            bonus_balance=bonus_balance
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, bonus_balance=0):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            bonus_balance=bonus_balance
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField('email', max_length=255, unique=True)
+    bonus_balance = models.PositiveIntegerField(
+        'bonus balance', default=0)
+    date_joined = models.DateTimeField('join date', auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        app_label = 'accounts'
--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
