97a6748cadf367fe5ff8bbca9f6dcf3fc6ba1e41
HerbyZ <andrey.tsuranoff@yandex.ru>
2022-01-17 21:08:39 +0300

    User ModelAdmin

--- bookshop/accounts/admin.py
+++ bookshop/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_joined', 'bonus_balance',
+                    'is_admin', 'is_active', 'is_staff', 'is_superuser')
+    fields = ('email', 'bonus_balance', 'date_joined', 'last_login',
+              'is_admin', 'is_active', 'is_staff', 'is_superuser')
+    readonly_fields = ('date_joined', 'last_login')
+
+
+admin.site.register(User, UserModelAdmin)
