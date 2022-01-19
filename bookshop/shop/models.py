f922b610bcb4bd8aba53ff8746e3edb43fc3e3a2
HerbyZ <andrey.tsuranoff@yandex.ru>
2022-01-19 22:17:21 +0300

    Book model

--- bookshop/shop/models.py
+++ bookshop/shop/models.py
@@ -27,3 +27,21 @@ class Genre(models.Model):
 
     class Meta:
         app_label = 'shop'
+
+
+class CoverMaterial(models.TextChoices):
+    HARDCOVER = ('HC', 'hardcover')
+    PAPERBACK = ('PB', 'paperback')
+
+
+class Book(Product):
+    name = models.CharField('name', max_length=255, default='')
+    description = models.TextField('description', default='')
+    genre = models.ForeignKey(
+        Genre, verbose_name='genre', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        Author, verbose_name='author', on_delete=models.CASCADE)
+    rating = models.FloatField('rating', default=0)
+    cover_material = models.CharField(
+        'cover material', max_length=20, choices=CoverMaterial.choices)
+    image = models.ImageField('image', upload_to='books')
