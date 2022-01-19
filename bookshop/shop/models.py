cb2410e44be302d5277f0dfe13b0f696ad903c16
HerbyZ <andrey.tsuranoff@yandex.ru>
2022-01-19 21:46:53 +0300

    Product abstract model

--- bookshop/shop/models.py
+++ bookshop/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('name', max_length=255, default='')
+    description = models.TextField('description', default='')
+    appear_date = models.DateTimeField('appear date', auto_now_add=True)
+    price = models.FloatField('price', default=0)
+
+    class Meta:
+        abstract = True
+        app_label = 'shop'
