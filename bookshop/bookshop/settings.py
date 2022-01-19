1cb396c212e0402c18a852f63f8517761b234ced
HerbyZ <andrey.tsuranoff@yandex.ru>
2022-01-19 21:58:13 +0300

    Media storage settings

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = Path(BASE_DIR).joinpath('media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
