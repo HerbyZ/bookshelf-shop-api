c4a2c469ff6afff39fd73f2d37f0ad486fb72986
HerbyZ <andrey.tsuranoff@yandex.ru>
2022-01-21 19:09:23 +0300

    Password and balance added to User model admin

--- bookshop/accounts/admin.py
+++ bookshop/accounts/admin.py
@@ -7,8 +7,8 @@ class UserModelAdmin(admin.ModelAdmin):
     list_display = ('email', 'date_joined', 'bonus_balance',
                     'is_admin', 'is_active', 'is_staff', 'is_superuser')
     fields = ('email', 'bonus_balance', 'date_joined', 'last_login',
-              'is_admin', 'is_active', 'is_staff', 'is_superuser')
-    readonly_fields = ('date_joined', 'last_login')
+              'is_admin', 'is_active', 'is_staff', 'is_superuser', 'balance', 'password')
+    readonly_fields = ('date_joined', 'last_login', 'password')
 
 
 admin.site.register(User, UserModelAdmin)
